97c057dada5ddce7e243ef1eb218813db9b39abb
M. Lee <mike.seung.lee@gmail.com>
2014-05-25 18:57:58 -0400

    Chapter 3.2
    More string exercises, mainly concatenation and multiplying strings

--- Programs/hello_world.rb
+++ Programs/hello_world.rb
@@ -1,3 +1,13 @@
 puts 'Hello, world!'
 puts ''
 puts 'Good-bye.'
+
+puts 'I like' + ' apple pie'
+puts 'I like ' + 'apple pie'
+
+puts 'blink ' * 4
+
+puts 12 + 12
+puts '12' + '12'
+puts '12' + 12
+puts '12 + 12'
