af6bfb8297949b0c0db5b3fd0047d9e4a01225ad
M. Lee <mike.seung.lee@gmail.com>
2014-05-28 22:39:59 -0400

    Chapter 4.11
    more practice with variables.  will skip the easy practice from now on
    if I know how to do it

--- Programs/simple_variables.rb
+++ Programs/simple_variables.rb
@@ -2,3 +2,9 @@ name = 'Anya Christina Emmanuella Jenkins Harris'
 puts 'My name is ' + name + '.'
 puts 'Wow! ' + name
 puts 'is a really long name!'
+
+composer = 'Mozart'
+puts composer + ' was "da bomb" in his day.'
+
+composer = 'Beethoven'
+puts 'But I prefer ' + composer + ', personally.'
