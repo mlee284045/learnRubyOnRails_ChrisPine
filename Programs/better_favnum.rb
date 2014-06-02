d9f35c951aad1a38d8c37cfc142c4615d218542a
M. Lee <mike.seung.lee@gmail.com>
2014-06-02 12:29:52 -0400

    Chapter 5.5 & 5.6
    Chomp method which removes the enter command from gets as well as the
    following exercises

---
+++ Programs/better_favnum.rb
@@ -0,0 +1,4 @@
+puts 'What\'s your favorite number?'
+orig_num = gets.chomp
+better_num = orig_num.to_i + 1
+puts 'How about the number, ' + better_num.to_s + '? I think it\'s a better number.'
---
+++ Programs/full_name.rb
@@ -0,0 +1,9 @@
+puts 'Hello there, and what\'s your first name?'
+first_name = gets.chomp
+puts 'How about your middle name?'
+middle_name = gets.chomp
+puts 'And your last name?'
+last_name = gets.chomp
+name = first_name +' '+ middle_name +' '+ last_name
+puts 'Your name is ' + name + '? What a lovely name!'
+puts 'Pleased to meet you, ' + name + '. :)'
