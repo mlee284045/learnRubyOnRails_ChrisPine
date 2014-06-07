542d675550ff68831c5fe35333b0523bb9a4abb7
M. Lee <mike.seung.lee@gmail.com>
2014-06-06 21:09:43 -0400

    Chapter 8.3b
    Change Table of Contents exercise to use new array methods

--- Programs/table_cont.rb
+++ Programs/table_cont.rb
@@ -1,15 +1,10 @@
 line_width = 60
 title = 'Table of Contents'
-chap_1 = ['Chapter 1', 'Getting Started', 'Page 1']
-chap_2 = ['Chapter 2', 'Numbers', 'Page 9']
-chap_3 = ['Chapter 3', 'Letters', 'Page 13']
-chap_4 = ['Chapter 4', 'Variables', 'Page 21']
+toc = [['Chapter 1', 'Chapter 2', 'Chapter 3', 'Chapter 4'], ['Getting Started', 'Numbers', 'Letters', 'Variables'], ['Page 1', 'Page 13', 'Page 34', 'Page 66']]
 
 puts(title.center(line_width))
 puts ''
-puts (chap_1[0].to_s.ljust(line_width/3) + chap_1[1].to_s.ljust(line_width/2) + chap_1[2].to_s.ljust(line_width/6))
-puts (chap_2[0].to_s.ljust(line_width/3) + chap_2[1].ljust(line_width/2) + chap_2[2].ljust(line_width/6))
-puts (chap_3[0].to_s.ljust(line_width/3) + chap_3[1].ljust(line_width/2) + chap_3[2].ljust(line_width/6))
-puts (chap_4[0].to_s.ljust(line_width/3) + chap_4[1].ljust(line_width/2) + chap_4[2].ljust(line_width/6))
-
-
+puts (toc[0][0].to_s.ljust(line_width/3) + toc[1][0].to_s.ljust(line_width/2) + toc[2][0].to_s.ljust(line_width/6))
+puts (toc[0][1].to_s.ljust(line_width/3) + toc[1][1].ljust(line_width/2) + toc[2][1].ljust(line_width/6))
+puts (toc[0][2].to_s.ljust(line_width/3) + toc[1][2].ljust(line_width/2) + toc[2][2].ljust(line_width/6))
+puts (toc[0][3].to_s.ljust(line_width/3) + toc[1][3].ljust(line_width/2) + toc[2][3].ljust(line_width/6))
